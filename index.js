6aaca9c9b82e912ffe4db9b238e75f05ed81698b
elvinmwarangu-stack <elvin.mwarangu@student.moringaschool.com>
2025-09-08 13:42:39 +0300

    commited files

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 2000
+let b = 100
+
+const divide = function(a, b) {
+    return a/b;
+}
+const square = (x) => x * x;
+
+let x = 2
+const add = (a, b) => a + b;
+
+console.log(divide(a, b));
+console.log(square(x));
+console.log(add(a, b));
