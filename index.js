778a26b4089bded60fe7d85a8844c3e8eb00e199
elvinmwarangu-stack <elvin.mwarangu@student.moringaschool.com>
2025-09-09 08:01:31 +0300

    commit a file

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// function expression
-const divide = function(a, b){
-    return a / b;
+const divide = function(a,b){
+    return a/b;
 }
-//arrow function
-const square = (x) => x*x;
-
-//arrow function
-const add = (a, b) => a + b;
+const square =x => x*x;
+const add = (a,b) => a+b;
 
 module.exports = {
     divide,
     square,
-    add,
-};
+    add
+}
